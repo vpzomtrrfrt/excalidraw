5e5d09cb8f063941084cc1b326dc100d2c2ab9c9
Colin Reeder <colin@vpzom.click>
2023-09-22 10:01:56 -0600

    Maybe fix laser pointer icon

--- src/shapes.tsx
+++ src/shapes.tsx
@@ -107,12 +107,13 @@ export const SHAPES = [
   },
   {
     icon: (
-      <svg viewBox="0 0 512 512">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M3 12h18"></path>
-        <path d="M12 21v-18"></path>
-        <path d="M7.5 7.5l9 9"></path>
-        <path d="M7.5 16.5l9 -9"></path>
+      <svg viewBox="0 0 24 24">
+        <g fill="none" stroke="currentColor">
+          <path d="M3 12h18"></path>
+          <path d="M12 21v-18"></path>
+          <path d="M7.5 7.5l9 9"></path>
+          <path d="M7.5 16.5l9 -9"></path>
+        </g>
       </svg>
     ),
     value: "laser",
